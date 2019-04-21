4563b9ba66a7b2360f2ecd1b91e152c7cefd0ed2
Stefan Maier <stefan.maier09@gmail.com>
2019-04-21 10:53:20 +0800

    Added all 46 Countries

--- searchads/country_code.go
+++ searchads/country_code.go
@@ -5,100 +5,200 @@ import (
 	"fmt"
 )
 
-// CountryCode type to represent enum of CountryCode (AU/CA)
+// CountryCode type to represent enum of CountryCode
 type CountryCode byte
 
-// AT, AU, BE, CA, CH, CZ, DE, DK, ES, FI, FR, GB, GR, HR, HU, IE, IT, JP, KR, MX, NL, NO, NZ, PT, RO, SE and US enum values
-// Anguilla Antigua and Barbuda Argentina Armenia Australia Austria Azerbaijan Bahamas Bahrain Barbados Belarus Belgium Belize Bermuda Bolivia Botswana Brazil British Virgin Islands Brunei Darussalam Bulgaria Burkina Faso Cambodia Canada Cape Verde Cayman Islands China Colombia Costa Rica Croatia Cyprus Czech Republic Denmark Dominica Dominican Republic Ecuador Egypt El Salvador Estonia Fiji Finland France Gambia Germany Gibraltar Greece Grenada Guatemala Guinea‑Bissau Honduras Hong Kong Hungary India Indonesia Ireland Israel Italy Japan Jordan Kazakhstan Kenya Korea, Republic of Kyrgyzstan Kuwait Lao People‘s Democratic Republic Latvia Lebanon Lithuania Luxembourg Macau Malaysia Malta Mayole Mexico Micronesia Moldova Mongolia Mozambique Namibia Nepal Netherlands New Zealand Nicaragua Niger Nigeria Norway Oman Pakistan Panama Papua New Guinea Paraguay Peru Philippines Poland Portugal Qatar Romania Saudi Arabia Singapore Slovakia Slovenia Spain Sri Lanka St. Kitts and Nevis Swaziland Sweden Switzerland Taiwan Tajikistan Thailand Trinidad and Tobago Turkey Turkmenistan Uganda Ukraine United Arab Emirates United Kingdom United States Uzbekistan Venezuela Vietnam Zimbabwe
+// CountryCode enum values for 46 countries (https://searchads.apple.com/countries-and-regions/)
 const (
-	AT CountryCode = iota
+	// Africa, Middle East, and India
+	EG CountryCode = iota
+	IN
+	IL
+	JO
+	KW
+	LB
+	OM
+	PK
+	QA
+	SA
+	ZA
+	AE
+	// Asia Pacific
 	AU
+	KH
+	HK
+	ID
+	JP
+	MO
+	MY
+	PH
+	NZ
+	SG
+	KR
+	TW
+	TH
+	VN
+	// Europe
+	AL
+	AT
+	AZ
 	BE
-	CA
-	CH
+	HR
 	CZ
-	DE
 	DK
-	ES
 	FI
 	FR
-	GB
+	DE
 	GR
-	HR
 	HU
 	IE
 	IT
-	JP
-	KR
-	MX
+	KZ
 	NL
 	NO
-	NZ
+	PL
 	PT
 	RO
+	ES
 	SE
+	CH
+	UA
+	GB
+	// Latin America and the Caribbean
+	AG
+	CO
+	CL
+	EC
+	MX
+	PE
+	// The United States, Canada, and Puerto Rico
 	US
+	CA
 )
 
 var (
 	_CountryCodeNameToValue = map[string]CountryCode{
+		"AG": AG,
+		"AL": AL,
+		"IL": IL,
 		"AT": AT,
 		"AU": AU,
+		"AZ": AZ,
+		"JO": JO,
 		"BE": BE,
+		"KW": KW,
+		"MO": MO,
+		"AE": AE,
+		"LB": LB,
+		"OM": OM,
+		"PK": PK,
+		"QA": QA,
+		"SA": SA,
+		"ZA": ZA,
 		"CA": CA,
 		"CH": CH,
+		"CO": CO,
+		"CL": CL,
 		"CZ": CZ,
 		"DE": DE,
 		"DK": DK,
+		"EC": EC,
+		"EG": EG,
 		"ES": ES,
 		"FI": FI,
 		"FR": FR,
 		"GB": GB,
 		"GR": GR,
+		"HK": HK,
 		"HR": HR,
 		"HU": HU,
 		"IE": IE,
+		"IN": IN,
+		"ID": ID,
 		"IT": IT,
 		"JP": JP,
+		"KH": KH,
 		"KR": KR,
+		"KZ": KZ,
 		"MX": MX,
+		"MY": MY,
 		"NL": NL,
 		"NO": NO,
 		"NZ": NZ,
-		"PT": PT,
+		"PE": PE,
 		"RO": RO,
-		"SE": SE,
+		"PH": PH,
+		"PL": PL,
+		"PT": PT,
 		"US": US,
+		"SE": SE,
+		"SG": SG,
+		"TW": TW,
+		"TH": TH,
+		"VN": VN,
+		"UA": UA,
 	}
 
 	_CountryCodeValueToName = map[CountryCode]string{
+		AG: "AG",
+		AL: "AL",
+		IL: "IL",
+		IN: "IN",
 		AT: "AT",
 		AU: "AU",
+		AZ: "AZ",
+		JO: "JO",
 		BE: "BE",
+		KW: "KW",
+		MO: "MO",
+		AE: "AE",
+		LB: "LB",
+		OM: "OM",
+		PK: "PK",
+		QA: "QA",
+		SA: "SA",
+		ZA: "ZA",
 		CA: "CA",
 		CH: "CH",
+		CO: "CO",
+		CL: "CL",
 		CZ: "CZ",
 		DE: "DE",
 		DK: "DK",
+		EC: "EC",
+		EG: "EG",
 		ES: "ES",
 		FI: "FI",
 		FR: "FR",
 		GB: "GB",
 		GR: "GR",
+		HK: "HK",
 		HR: "HR",
 		HU: "HU",
 		IE: "IE",
+		ID: "ID",
 		IT: "IT",
 		JP: "JP",
+		KH: "KH",
 		KR: "KR",
+		KZ: "KZ",
 		MX: "MX",
+		MY: "MY",
 		NL: "NL",
 		NO: "NO",
 		NZ: "NZ",
-		PT: "PT",
+		PE: "PE",
 		RO: "RO",
-		SE: "SE",
+		PH: "PH",
+		PL: "PL",
+		PT: "PT",
 		US: "US",
+		SE: "SE",
+		SG: "SG",
+		TW: "TW",
+		TH: "TH",
+		VN: "VN",
+		UA: "UA",
 	}
 )
 
@@ -106,33 +206,65 @@ func init() {
 	var v CountryCode
 	if _, ok := interface{}(v).(fmt.Stringer); ok {
 		_CountryCodeNameToValue = map[string]CountryCode{
+			interface{}(AG).(fmt.Stringer).String(): AG,
+			interface{}(AL).(fmt.Stringer).String(): AL,
+			interface{}(IL).(fmt.Stringer).String(): IL,
+			interface{}(IN).(fmt.Stringer).String(): IN,
 			interface{}(AT).(fmt.Stringer).String(): AT,
 			interface{}(AU).(fmt.Stringer).String(): AU,
+			interface{}(AZ).(fmt.Stringer).String(): AZ,
+			interface{}(JO).(fmt.Stringer).String(): JO,
 			interface{}(BE).(fmt.Stringer).String(): BE,
+			interface{}(MO).(fmt.Stringer).String(): MO,
+			interface{}(KW).(fmt.Stringer).String(): KW,
+			interface{}(AE).(fmt.Stringer).String(): AE,
+			interface{}(ZA).(fmt.Stringer).String(): ZA,
+			interface{}(LB).(fmt.Stringer).String(): LB,
+			interface{}(OM).(fmt.Stringer).String(): OM,
+			interface{}(PK).(fmt.Stringer).String(): PK,
+			interface{}(QA).(fmt.Stringer).String(): QA,
+			interface{}(SA).(fmt.Stringer).String(): SA,
 			interface{}(CA).(fmt.Stringer).String(): CA,
 			interface{}(CH).(fmt.Stringer).String(): CH,
+			interface{}(CO).(fmt.Stringer).String(): CO,
+			interface{}(CL).(fmt.Stringer).String(): CL,
 			interface{}(CZ).(fmt.Stringer).String(): CZ,
 			interface{}(DE).(fmt.Stringer).String(): DE,
 			interface{}(DK).(fmt.Stringer).String(): DK,
+			interface{}(EC).(fmt.Stringer).String(): EC,
+			interface{}(EG).(fmt.Stringer).String(): EG,
 			interface{}(ES).(fmt.Stringer).String(): ES,
 			interface{}(FI).(fmt.Stringer).String(): FI,
 			interface{}(FR).(fmt.Stringer).String(): FR,
 			interface{}(GB).(fmt.Stringer).String(): GB,
 			interface{}(GR).(fmt.Stringer).String(): GR,
+			interface{}(HK).(fmt.Stringer).String(): HK,
 			interface{}(HR).(fmt.Stringer).String(): HR,
 			interface{}(HU).(fmt.Stringer).String(): HU,
 			interface{}(IE).(fmt.Stringer).String(): IE,
+			interface{}(ID).(fmt.Stringer).String(): ID,
 			interface{}(IT).(fmt.Stringer).String(): IT,
 			interface{}(JP).(fmt.Stringer).String(): JP,
+			interface{}(KH).(fmt.Stringer).String(): KH,
 			interface{}(KR).(fmt.Stringer).String(): KR,
+			interface{}(KZ).(fmt.Stringer).String(): KZ,
 			interface{}(MX).(fmt.Stringer).String(): MX,
+			interface{}(MY).(fmt.Stringer).String(): MY,
 			interface{}(NL).(fmt.Stringer).String(): NL,
 			interface{}(NO).(fmt.Stringer).String(): NO,
 			interface{}(NZ).(fmt.Stringer).String(): NZ,
-			interface{}(PT).(fmt.Stringer).String(): PT,
+			interface{}(PE).(fmt.Stringer).String(): PE,
 			interface{}(RO).(fmt.Stringer).String(): RO,
-			interface{}(SE).(fmt.Stringer).String(): SE,
+			interface{}(PT).(fmt.Stringer).String(): PT,
+			interface{}(PH).(fmt.Stringer).String(): PH,
+			interface{}(PL).(fmt.Stringer).String(): PL,
 			interface{}(US).(fmt.Stringer).String(): US,
+			interface{}(SE).(fmt.Stringer).String(): SE,
+			interface{}(SG).(fmt.Stringer).String(): SG,
+			interface{}(TW).(fmt.Stringer).String(): TW,
+			interface{}(TH).(fmt.Stringer).String(): TH,
+			interface{}(VN).(fmt.Stringer).String(): VN,
+			interface{}(UA).(fmt.Stringer).String(): UA,
 		}
 	}
 }
