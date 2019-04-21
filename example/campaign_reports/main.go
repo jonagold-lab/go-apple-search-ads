dc4a78915eb92c0aa3f55376d6934c067bcd553b
Stefan Maier <stefan.maier09@gmail.com>
2019-04-21 18:15:11 +0800

    Added Campaign Report example

---
+++ example/campaign_reports/main.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"log"
+
+	"github.com/jonagold-lab/go-apple-search-ads/searchads"
+)
+
+func main() {
+	orgID := 378940
+	client, err := searchads.NewClient(nil, "../cert.pem", "../cert.key", &orgID)
+	if err != nil {
+		log.Fatalf("Client error: %s", err)
+		panic(err)
+	}
+	filter := searchads.ReportFilter{
+		StartTime:   "2019-04-01",
+		EndTime:     "2019-04-05",
+		Granularity: searchads.HOURLY,
+		Selector: searchads.Selector{
+			OrderBy: []searchads.OrderBy{
+				searchads.OrderBy{
+					Field:     "countryOrRegion",
+					SortOrder: searchads.ASCENDING,
+				},
+			},
+			Conditions: []searchads.Condition{
+				searchads.Condition{
+					Field:    "countriesOrRegions",
+					Operator: searchads.OperatorContainsAny,
+					Values:   []string{"US", "GB"},
+				},
+			},
+			Pagination: searchads.FilterPagination{
+				Offset: 0,
+				Limit:  1000,
+			},
+		},
+		GroupBy:                    []string{"countryOrRegion"},
+		TimeZone:                   searchads.UTC,
+		ReturnRecordsWithNoMetrics: true,
+		ReturnRowTotals:            true,
+		ReturnGrandTotals:          true,
+	}
+	report, rs, err := client.Report.Campaigns(context.Background(), &filter)
+	if err != nil {
+		log.Fatalf("Campaign Reports error: %s", err)
+		panic(err)
+	}
+	res, _ := json.Marshal(&report)
+	fmt.Println(string(res))
+	fmt.Println("----------------")
+	fmt.Println(report.ReportingDataResponse.Row)
+	fmt.Println(rs.Pagination.ItemsPerPage)
+	fmt.Println(rs.Pagination.StartIndex)
+	fmt.Println(rs.Pagination.TotalResults)
+}
